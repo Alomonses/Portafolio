93ae5eab41ff4bcdeb323c1923a20c78f169ef23
Alondra Sanchez <alomonse1426@gmail.com>
2023-08-25 01:46:24 -0700

    first commit

---
+++ index.js
@@ -0,0 +1,15 @@
+//TYPED
+var typed = new Typed('#typed', {
+    strings: ["Web Developer", "MERN Stack Developer"],
+    stringselement: null,
+    typedSpeed: 20,
+    startDelay: 150,
+    backSpeed: 20,
+    backDelay: 1000,
+    loop:true,
+    loopCount: 550,
+    showCursor: true,
+    cursosrChar: "|",
+    atrr:null,
+    contentTypeL:"html",
+  });
